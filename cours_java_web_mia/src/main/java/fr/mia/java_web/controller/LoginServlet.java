77f258ef1d00a865cc241a625f5b5e6bf959ae49
mia <mialy.info@gmail.com>
2020-07-08 16:15:04 +0200

    Paramètres de la page login récupérés

--- cours_java_web_mia/src/main/java/fr/mia/java_web/controller/LoginServlet.java
+++ cours_java_web_mia/src/main/java/fr/mia/java_web/controller/LoginServlet.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
-    private static final String TEST_SERVLET = "LA SERVLET EST OK";
+    //private static  String login = "LA SERVLET LOGIN EST OK";
+    //private static  String password = "LA SERVLET LOGIN EST OK";
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -17,9 +18,10 @@ public class LoginServlet extends HttpServlet {
 
         // Actual logic goes here
         PrintWriter out = resp.getWriter();
-        out.println("Login done");
+        out.println("Login done  " +  req.getParameter("login") + " PASSWORD  " + req.getParameter("password"));
 
         // Récupérer les parametres du formulaire en demandant a la requete
-
+             req.getParameter("login");
+             req.getParameter("password");
     }
 }
